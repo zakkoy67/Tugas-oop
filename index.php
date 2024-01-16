cb07aed6a180eb0eed115f95139625b80f75ef2e
zakkoy67 <118705766+zakkoy67@users.noreply.github.com>
2024-01-16 17:53:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,24 @@ require_once "frog.php";
 require_once "ape.php";
 require_once "animal.php";
 
-$sheep = new animal("shaun");
-echo "Name: " . $sheep->get_name();
-echo "<br/>legs:". $sheep->get_legs();
-echo "<br/> cold blooded: ". $sheep->get_blood();
+$sheep = new animal();
+echo "Name: " . $sheep->get_name() . "<br>";
+echo "legs:". $sheep->get_legs() . "<br>";
+echo "cold blooded: ". $sheep->get_blood() . "<br>";
 
-$kodok = new frog("buduk");
-$kodok->getfrogname();
-$kodok->getfroglegs();
-$kodok->getfrogblood();
-$kodok->getfrogjump();
+echo "<br>";
 
-$ape = new ape("kera sakti");
-$ape->getapename();
-$ape->getapelegs();
-$ape->getapeblood();
-$ape->getapeyell();
+$kodok = new frog();
+echo "Name: " . $kodok->getfrogname() . "<br>";
+echo "legs:".$kodok->getfroglegs() . "<br>";
+echo "cold blooded: ".$kodok->getfrogblood() . "<br>";
+echo "jump: " . $kodok->getfrogjump() . "<br>";
+
+echo "<br>";
+
+$ape = new ape();
+echo "Name: " .$ape->getapename() . "<br>";
+echo "legs:".$ape->getapelegs() . "<br>";
+echo "cold blooded: ".$ape->getapeblood() . "<br>";
+echo "Yell:   " .  $ape->getapeyell() . "<br>";
 ?>
