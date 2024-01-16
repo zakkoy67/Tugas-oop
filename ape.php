c01c3429b60ac295d7e68d978f49089f804555b2
zakkoy67 <118705766+zakkoy67@users.noreply.github.com>
2024-01-16 17:52:48 +0700

    Update ape.php

--- ape.php
+++ ape.php
@@ -2,15 +2,15 @@
 require_once "Animal.php";
 class ape extends animal{
     public function getapename(){
-        echo"<br/><br/>name: goku";
+        return "Kera sakti";
     }
     public function getapelegs(){
-        echo"<br/>legs : 2";
+        return "2";
     }
     public function getapeblood(){
-        echo"<br/>cold blooded: no";
+        return "no";
     }
     public function getapeyell(){
-        return"auoauo";
+        return"Auooo";
     }
 }
